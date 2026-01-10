c3574f719b4aed3d8e626c7caa066e6f3b9452f4
jshivanshuu <jshivanshu582@gmail.com>
2026-01-10 13:25:56 +0530

    made the form working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,6 +6,12 @@ def hello_world():
     return "<p>Hello, World!</p>"
 @app.route("/login",methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        print(username,password)
+        return render_template("login.html")
     return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
