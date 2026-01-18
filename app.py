cd0a9d2123887b9f803a8db4d198adf41401ead8
jshivanshuu <jshivanshu582@gmail.com>
2026-01-18 10:22:57 +0530

    fixed plan variable issue

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, session
 from Entry.observer import observe_request
 from deception.response_engine import decide
-from deception.messages import get_failure_message
 import time
+from flask import redirect
 app = Flask(__name__)
 secret_key = "Falsefront"
 app.secret_key = secret_key
@@ -18,19 +18,20 @@ def login():
         session["last_attempt_time"] = time.time()
     session_id = session["id"]
     observe_request(request,session_id)
-    if request.method == "POST":
-        session["attempts"] += 1
+    if request.method == "GET":
+        return render_template("login.html")
+    session["attempts"] += 1
 
-        plan = decide({
-            "attempts": session["attempts"],
-            "last_attempt_time": session["last_attempt_time"]
-        })
+    plan = decide({
+        "attempts": session["attempts"],
+        "last_attempt_time": session["last_attempt_time"]
+    })
 
-        session["last_attempt_time"] = time.time()
-        if plan["delay"] > 0:
-            time.sleep(plan["delay"])
-        if plan["fake_success"]:
-            return redirect("/dashboard")
+    session["last_attempt_time"] = time.time()
+    if plan["delay"] > 0:
+        time.sleep(plan["delay"])
+    if plan["fake_success"]:
+        return redirect("/dashboard")
 
     return render_template("login.html",error_message=plan["message"])
 @app.route("/dashboard")
