c28fa6c991a8d9d4db9db110aaae5e72832d2275
jshivanshuu <jshivanshu582@gmail.com>
2026-01-21 11:41:23 +0530

    modified gitignore and response_engine

--- deception/fakesuccess.py
+++ deception/fakesuccess.py
@@ -1,5 +1,5 @@
 def fakeadminlogin(attempt_count, time_since_last):
-    if attempt_count >= 3 and time_since_last < 1:
+    if attempt_count >= 3 and time_since_last < 9:
         return True
     else:
         return False
